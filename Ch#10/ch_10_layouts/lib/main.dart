8d7080102dd6b3727f226e7e8128129dc1b4e353
Ibrahim <m.ibrahimab30139@gmail.com>
2023-10-16 14:18:40 +0500

    Done with ch#10 (birthday layout)

---
+++ Ch#10/ch_10_layouts/lib/main.dart
@@ -0,0 +1,25 @@
+
+import 'package:ch_10_layouts/Pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Starter Template',
+        
+      theme: ThemeData(
+
+        primarySwatch: Colors.amber,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+
